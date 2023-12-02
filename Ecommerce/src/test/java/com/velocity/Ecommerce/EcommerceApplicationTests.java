c0c3f252a6fdd594b998c7e5022b84d0441ce1b7
m4vinayak <m4vinayak@gmail.com>
2023-12-02 18:33:53 +0530

    Ecommerce Project Initialized successfully

---
+++ Ecommerce/src/test/java/com/velocity/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velocity.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
